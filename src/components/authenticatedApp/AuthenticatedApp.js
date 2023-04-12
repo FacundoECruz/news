233a2cf27ec6ccd33992753680727c65a4a13f36
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-12 16:15:30 -0300

    receiving data from api

--- src/components/authenticatedApp/AuthenticatedApp.js
+++ src/components/authenticatedApp/AuthenticatedApp.js
@@ -1,19 +1,40 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from "react";
+import { client } from "../../utils/api-client";
 
 function AuthenticatedApp() {
 
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState("")
+  const [queried, setQueried] = useState(false)
+  const [status, setStatus] = useState("idle")
+  const [data, setData] = useState(null);
   const inputRef = useRef()
 
-  console.log(query)
+  console.log(data)
+
+  useEffect(() => {
+    if(!queried) {
+      return
+    }
+    setStatus("loading")
+    client(`everything?q=${encodeURIComponent(query)}`).then(responseData => {
+      setData(responseData)
+      setStatus("success")
+    })
+  }, [query, queried])
+
+  function handleSearchSubmit(e) {
+    e.preventDefault()
+    setQueried(true)
+    setQuery(inputRef.current.value)
+  }
 
   return ( 
     <>
-      <div>
-        <label htmlFor='query'>Search: </label>
+      <form onSubmit={handleSearchSubmit}>
+        <label htmlFor="query">Search: </label>
         <input type="text" id="query" ref={inputRef}/>
-        <button onClick={(e) => setQuery(inputRef.current.value)}>Go</button>
-      </div>
+        <button type="submit">Go</button>
+      </form>
     </>
    );
 }
---
+++ src/utils/api-client.js
@@ -0,0 +1,15 @@
+function client(endpoint, customConfig = {}) {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: "15b9600598e84ed393d3b53268f305b2",
+    },
+    ...customConfig,
+  };
+
+  return window
+    .fetch(`https://newsapi.org/v2/${endpoint}`, config)
+    .then((response) => response.json());
+}
+
+export { client };
