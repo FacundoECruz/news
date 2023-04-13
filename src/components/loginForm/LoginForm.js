78f937b361a7ed1c17e7596d665844bf2be45bea
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-13 12:34:05 -0300

    styled login form

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -1,29 +1,32 @@
-function LoginForm({ onSubmit, buttonText, error }) {
+import "./LoginForm.css";
 
+function LoginForm({ onSubmit, buttonText, error }) {
   function handleSubmit(e) {
-    e.preventDefault()
-    const {username, password} = e.target.elements
+    e.preventDefault();
+    const { username, password } = e.target.elements;
 
     onSubmit({
       username: username.value,
       password: password.value,
-    })
+    });
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
+      <div className="username-container">
         <label htmlFor="username">Username</label>
         <input type="text" id="username" />
       </div>
-      <div>
+      <div className="password-container">
         <label htmlFor="password">Password</label>
         <input type="password" id="password" />
       </div>
-      <div>
-        <button type="submit">{buttonText}</button>
+      <div className="submit-container">
+        <button className="submit-btn" type="submit">
+          {buttonText}
+        </button>
       </div>
-      {error ? error : null}
+      <div className="error-display">{error ? error : null}</div>
     </form>
   );
 }
