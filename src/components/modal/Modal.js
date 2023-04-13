855fa90468b1489765d3f782481d6bed06028fb6
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-13 12:24:44 -0300

    styled modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import LoginForm from '../loginForm/LoginForm';
-import './Modal.css'
+import React from "react";
+import LoginForm from "../loginForm/LoginForm";
+import "./Modal.css";
 
-function Modal({onSubmit, buttonText, close, error}) {
-
-  return(
-    <div className='modal'>
-      <div className='modal-content'>
-        <div className='modal-header'>
-          <h4 className='modal-title'>{buttonText}</h4>
+function Modal({ onSubmit, buttonText, close, error }) {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h4 className="modal-title">{buttonText}</h4>
+          <div className="modal-close">
+            <button className="modal-close-btn" onClick={() => close()}>x</button>
+          </div>
         </div>
-        <div className='modal-body'>
-          <LoginForm onSubmit={onSubmit} buttonText={buttonText} error={error}/>
-        </div>
-        <div className='modal-footer'>
-          <button onClick={() => close()}>Close</button>
+        <div className="modal-body">
+          <LoginForm
+            onSubmit={onSubmit}
+            buttonText={buttonText}
+            error={error}
+          />
         </div>
       </div>
     </div>
-  )
-} 
+  );
+}
 
 export default Modal;
