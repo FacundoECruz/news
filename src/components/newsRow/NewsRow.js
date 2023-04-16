79aad7eb99783e20de7e63ce32789fc60702b551
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-16 09:56:04 -0300

    added default img

--- src/components/newsRow/NewsRow.js
+++ src/components/newsRow/NewsRow.js
@@ -16,12 +16,18 @@ function NewsRow({ data }) {
     <div className="news-row-container">
       <div className="news-header">
         <h3 className="news-title">{title}</h3>
-        {author 
-        ? <p className="news-author">{author}</p>
-        : null}
-        <p>{source.name}</p>
+        {author ? <p className="news-author">{author}</p> : null}
+        <p className="news-source">{source.name}</p>
       </div>
-      <img src={urlToImage} alt="news-img" className="news-img" />
+      {urlToImage ? (
+        <img src={urlToImage} alt="news-img" className="news-img" />
+      ) : (
+        <img
+          src="https://media.istockphoto.com/id/187925868/vector/newspaper-cover-page.jpg?s=612x612&w=0&k=20&c=SantJnFi_0dCOD_HUXgRSJxagvgL7Wp_F_e-rFSUV_E="
+          alt="news-img"
+          className="news-img"
+        />
+      )}
     </div>
   );
 }
